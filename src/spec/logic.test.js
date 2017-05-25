5094d0d3e7a5909772b5cdf30cc309b24d280e32
Guillaume Bouffard <guillaume@getkahoot.com>
2017-05-25 10:19:21 +0100

    Move logic unit test from Jasmine testing to Chai

--- src/spec/logic.test.js
+++ src/spec/logic.test.js
@@ -1,8 +1,20 @@
 import logic from '../logic';
+import chai, { expect } from 'chai'
 
 describe('logic - ', function() {
+  it('is not empty', () => {
+    expect(logic).not.to.be.empty;
+    expect(logic).not.to.have.property('foo');
+    expect(logic).to.have.property('sum');
+  });
+
   it('sum function sums numbers', () => {
-    expect(logic.sum(1, 2)).toEqual(3);
-    expect(logic.sum(2, 2)).toEqual(4);
+    // This was jasmine testing
+    // expect(logic.sum(1, 2)).toEqual(3);
+    // expect(logic.sum(2, 2)).toEqual(4);
+
+    // but using Chai
+    expect(logic.sum(1, 2)).to.equal(3);
+    expect(logic.sum(2, 2)).to.equal(4);
   });
  });
