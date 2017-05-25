e2ba4da490b90a69707c5b27cd180f50b8d924b9
Guillaume Bouffard <guillaume@getkahoot.com>
2017-05-25 09:44:34 +0100

    Use Chai for test - main App - renders

--- src/spec/App.test.js
+++ src/spec/App.test.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import { shallow } from 'enzyme';
+import chai, { expect } from 'chai'
+import chaiEnzyme from 'chai-enzyme';
 import App from '../App';
 
-it('renders welcome message', () => {
-  const wrapper = shallow(<App />);
-  const welcome = <h2>Welcome to React</h2>;
-  // expect(wrapper.contains(welcome)).to.equal(true);
-  expect(wrapper.contains(welcome)).toEqual(true);
+describe('main App - ', () => {
+  chai.use(chaiEnzyme);
+  const appWrapper = shallow(<App />);
+
+  it('renders', () => {
+    expect(appWrapper).to.have.length(1);
+    expect(appWrapper).to.have.length.above(0);
+    expect(appWrapper).to.have.length.of.at.least(1);
+    expect(appWrapper).to.have.length.below(4);
+    expect(appWrapper).to.have.length.of.at.most(3);
+  });
+
+  xit('renders the welcome message', () => {
+  });
 });
