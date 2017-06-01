59a8f16a15c1b776838f4648c5e94f4df3ced3a8
Guillaume Bouffard <guillaume@getkahoot.com>
2017-06-01 15:40:14 +0100

    Test Chai instance of on the instance of a class

--- src/spec/dog.spec.js
+++ src/spec/dog.spec.js
@@ -2,6 +2,9 @@ import myDog from '../dog';
 import chai, { expect } from 'chai'
 
 describe('myDog class', () => {
+  const max = new myDog();
+  const maxbarksInTheGarden = max.barksInTheGarden();
+
   describe('constructor function', () => {
     it('should respond', () => {
       expect(myDog).to.respondTo('constructor')
@@ -14,8 +17,20 @@ describe('myDog class', () => {
     });
 
     it('works', () => {
-      const dog = new myDog();
-      expect(dog.barksInTheGarden()).to.equal('Woof! Woof! WOOF!!');
+      expect(maxbarksInTheGarden).to.be.a('string');
+      expect(maxbarksInTheGarden).to.equal('Woof! Woof! WOOF!!');
+    });
+  });
+
+  describe('instance of', () => {
+    it('testing the instance of myDog', () => {
+      expect(max).to.be.an.instanceof(myDog);
+    });
+
+    it('testing that the instance responds to the barksInTheGarden function', () => {
+      expect(max).to.respondTo('barksInTheGarden');
+      expect(max).not.to.respondTo('miaowInTheKitchen');
+      expect(max).to.be.an('object').that.respondsTo('barksInTheGarden');
     });
   });
 });
