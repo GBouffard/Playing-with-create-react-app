78a6f20dc34fff6930f07f7a283c67c0d76be4f7
Guillaume Bouffard <guillaume@getkahoot.com>
2017-06-23 16:24:01 +0100

    Change language button - Unit test - Snapshot test

---
+++ src/spec/changeLanguageButton.spec.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import Button from '../Button';
+import App from '../App';
+import renderer from 'react-test-renderer';
+import { shallow } from 'enzyme';
+
+describe('Change language Button', () => {
+  const mockChangeLanguage = jest.fn();
+
+  describe('Unit Test', () => {
+    it('renders and matches its snapshot', () => {
+      // Using renderer.create from react-test-renderer to test a snapshot.
+      const component = renderer.create(
+        <Button
+          text="Language"
+          className="whatever"
+          onClick={mockChangeLanguage} />
+      );
+      let tree = component.toJSON();
+      expect(tree).toMatchSnapshot();
+    });  
+  });
+});
