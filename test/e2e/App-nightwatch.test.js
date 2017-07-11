e7c40833f20814fd05ed56ff82234164e2eed5e2
Guillaume Bouffard <guillaume@getkahoot.com>
2017-07-11 10:55:31 +0100

    Testing the Thulium link

--- test/e2e/App-nightwatch.test.js
+++ test/e2e/App-nightwatch.test.js
@@ -19,6 +19,15 @@ module.exports = {
       .waitForElementNotVisible('#hideMe', 1000)
       // .hidden .Checks if the given element is not visible on the page.
       .assert.hidden('#hideMe')
+  },
+
+  'Testing the Thulium link': function(browser) {
+    browser
+      .url('http://localhost:3000/')
+      .waitForElementVisible('body', 1000)
+      .click('.normal')
+      .pause(1000)
+      .assert.urlEquals("http://www.thulium69.com/")
       // end () ends the session and close the window
       .end();
   }
