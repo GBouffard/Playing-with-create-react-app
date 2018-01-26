e0bb19349e85457b7ec3b875b71aafaa048c1204
Guillaume Bouffard <guillaume@getkahoot.com>
2018-01-26 15:36:19 +0000

    Remove App intro

--- src/App.js
+++ src/App.js
@@ -46,10 +46,6 @@ export default class App extends Component {
           <h2>Welcome to React</h2>
         </div>
 
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-
         <div>
           <Button
             text="HIDE ME"
