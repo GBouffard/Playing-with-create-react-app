f8bcd98c40487527920919edf72e56353d62bd0e
Guillaume Bouffard <guillaume@getkahoot.com>
2018-01-30 10:14:58 +0000

    Call BrowserRouter

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 import MainPage from './main-page';
 
 ReactDOM.render(
-  <MainPage />,
+  (
+    <BrowserRouter>
+      <Route path="/" component={MainPage}/>
+    </BrowserRouter>
+  ),
   document.getElementsByTagName('body')[0]
 );
