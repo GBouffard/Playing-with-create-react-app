bc3d1564bd336579ccca79e082036167549a0175
Guillaume Bouffard <guillaume@getkahoot.com>
2018-01-30 10:32:45 +0000

    Avoid rendering within body directly

--- src/App.js
+++ src/App.js
@@ -3,11 +3,15 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter, Route } from 'react-router-dom';
 import MainPage from './main-page';
 
+const root = document.createElement('div');
+root.id = 'guillaume-app';
+document.body.appendChild(root);
+
 ReactDOM.render(
   (
     <BrowserRouter>
       <Route path="/" component={MainPage}/>
     </BrowserRouter>
   ),
-  document.getElementsByTagName('body')[0]
+  document.getElementById('guillaume-app')
 );
