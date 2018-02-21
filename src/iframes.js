c6cc68c4de3148b54577cedcb7a95f361885e146
Guillaume Bouffard <guillaume@getkahoot.com>
2018-02-21 23:58:23 +0000

    Add iframes examples

--- src/iframes.js
+++ src/iframes.js
@@ -3,7 +3,21 @@ import React from 'react';
 const IFrames = () => {
   return (
     <div>
-      Still to come
+
+      <iframe
+        width="800"
+        height="300"
+        src="https://www.metallica.com/">
+      </iframe> 
+ 
+      <iframe
+        width="420"
+        height="315"
+        src="http://www.youtube.com/embed/uNNlTvviUZc"
+        frameborder="0"
+        allowfullscreen>
+      </iframe>
+
     </div>
   );
 }
