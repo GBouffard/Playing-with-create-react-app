4e85b2d21b734077ff562f20931df1196e5b6664
Guillaume Bouffard <guillaume@getkahoot.com>
2018-02-21 22:53:46 +0000

    Add a compiled string example within paragraphs

--- src/Paragraphs.js
+++ src/Paragraphs.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Paragraph from './paragraph'
+import _ from 'lodash';
 
 const Paragraphs = () => {
   return (
@@ -15,8 +16,32 @@ const Paragraphs = () => {
         children={"This is a paragraph sends a modifier and a className"}
         className="paragraph--fantasy"
         type="large" />
+
+      <Paragraph
+        children={"This is a paragraph sends a modifier and a className"}
+        className="paragraph--fantasy"
+        type="large" />
+
+      <div
+        className="string-literal-example"
+        dangerouslySetInnerHTML={{__html: compiledStringLiteral}} />
+
     </div>
   );
 }
+
+// THIS PART IS FOR THE COMPILED STRING EXAMPLE
+const stringLiteral = `This is a compiled message from string literal.
+  As example, through innerHTML, I can pass a link, make it bold and
+  I pass <b><a href="<%= myLink %>"> with a link</a></b> and my nickname
+  which is <i><b><%= nickName %></b></i>.`;
+
+const values = {
+  myLink: 'https://github.com/GBouffard',
+  nickName: 'Boubou'
+};
+
+const compiled = _.template(stringLiteral);
+const compiledStringLiteral = compiled(values);
 
 export default Paragraphs;
