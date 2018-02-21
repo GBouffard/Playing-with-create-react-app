d4f66ebb1ccfb97570dc8728fe4f1077f3fb7d17
Guillaume Bouffard <guillaume@getkahoot.com>
2018-02-21 17:22:56 +0000

    Add some Paragraph within Paragraphs

--- src/Paragraphs.js
+++ src/Paragraphs.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import Paragraph from './paragraph'
 
 const Paragraphs = () => {
   return (
     <div>
-      Still to come
+      <Paragraph
+        children="This is an example paragraph" />
+
+      <Paragraph
+        children={"This is a paragraph sends a modifier"}
+        type="large" />
+
+      <Paragraph
+        children={"This is a paragraph sends a modifier and a className"}
+        className="paragraph--fantasy"
+        type="large" />
     </div>
   );
 }
