715179a8b1cf2c45f95162d001674196ae0fcc9a
Guillaume Bouffard <guillaume@getkahoot.com>
2018-02-22 00:27:57 +0000

    Add first round of SVGs shapes

--- src/SVGs.js
+++ src/SVGs.js
@@ -1,9 +1,54 @@
 import React from 'react';
 
+const styledDiv = {
+  display: 'flex'
+};
+
 const SVGs = () => {
   return (
     <div>
-      Still to come
+      <h2>Some drawn shapes made in SVGs:</h2>
+
+      <div
+        style={styledDiv}>
+
+        <svg>
+          <rect
+            height={50}
+            width={200}
+            x={25}
+            y={25}
+            fill="lightseagreen" />
+        </svg>
+
+        <svg>
+          <rect
+            x={15}
+            y={15}
+            width={70}
+            height={70}
+            stroke="red"
+            strokeWidth={2}
+            fill="orange" />
+        </svg>
+
+        <svg>
+          <ellipse
+            cx={50}
+            cy={50}
+            rx={50}
+            ry={25}
+            fill="mediumorchid" />
+        </svg>
+
+        <svg>
+          <circle
+            cx={50}
+            cy={50}
+            r={30}
+            fill="red" />
+        </svg>
+      </div>
     </div>
   );
 }
