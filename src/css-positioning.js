3cc26f8c072e236c690cd916415252ac2e6e0dcc
Guillaume Bouffard <guillaume@getkahoot.com>
2018-02-28 09:30:41 +0000

    Add different positioning as examples

--- src/css-positioning.js
+++ src/css-positioning.js
@@ -5,27 +5,34 @@ const CSSPositioning = () => {
     <div>
       <div
         className="positioning positioning--static">
-        This div's position is static
+        This div's position is static (default value)<br/>
+        (within the normal document flow)
       </div>
 
       <div
         className="positioning positioning--relative">
-        This div's position is relative
+        This div's position is relative (relative to its own position)<br/>
+        (within the normal document flow)
       </div>
 
       <div
         className="positioning positioning--absolute">
-        This div's position is absolute
+        This div's position is absolute (relative to its parent/html)<br/>
+        OUT of the document flow<br/>
+        Also, though absolutely positioned boxes have MARGINS
       </div>
 
       <div
         className="positioning positioning--fixed">
-        This div's position is fixed
+        This div's position is fixed (similar to absolute HOWEVER unaffected by scrolling)<br/>
+        OUT of the document flow<br/>
+        Also, just like absolute positioned boxes, fixed boxes have MARGINS
       </div>
 
       <div
         className="positioning positioning--sticky">
-        This div's position is sticky
+        This div's position is sticky (behaves like a relative until it reaches a specified threshold, at which it behaves like fixed)<br/>
+        (within the normal document flow)
       </div>
 
       <br/>
