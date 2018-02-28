787b00b05ae1ddf6e698516eb7f0e2cb3d3593ba
Guillaume Bouffard <guillaume@getkahoot.com>
2018-02-28 20:55:18 +0000

    Add z-index examples

--- src/css-positioning.js
+++ src/css-positioning.js
@@ -61,9 +61,20 @@ const CSSPositioning = () => {
         In its simplest use, the float property can be used to wrap text around images. In this example the floating duck is on the right but it can be moved on the left with float: left.
       </div>
 
+      <div
+        className="positioning-z-index positioning--absolute positioning--z-index-1">
+        A position absolute div...by default under the next div<br/>
+        but not with a positive z-index.
+      </div>
+
+      <div
+        className="positioning-z-index positioning--relative">
+        A position relative div to illustrate z-index<br/>
+        z-index will only work on an element whose position property has been explicitly set to absolute, fixed, or relative. 
+      </div>
+
       <br/>
       Still to come: <br/>
-      - z-index<br/>
       - Box Model<br/>
       - inline vs inline-block<br/>
       - div style="clear:both; / Clearfix method / overflow: auto or overflow: hidden<br/>
