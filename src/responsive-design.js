29ad9ba11b7d103c0ed4709797ef4b4f90792584
Guillaume Bouffard <guillaume@getkahoot.com>
2018-03-01 18:38:34 +0000

    Add headings for responsive and adaptive designs

--- src/responsive-design.js
+++ src/responsive-design.js
@@ -3,7 +3,21 @@ import React from 'react';
 const ResponsiveDesign = () => {
   return (
     <div>
-      Still to come
+      <h3>
+        Responsive Design:
+      </h3>
+
+      <h3>
+        Adaptive Design:
+      </h3>
+
+      <h3>
+        Retina graphics Design:
+      </h3>
+
+      <h3>
+        - even if not CSS, html lang="en"
+      </h3>
     </div>
   );
 }
