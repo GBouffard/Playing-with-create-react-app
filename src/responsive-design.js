afad8758c112ab3ec1137343436b441132ae144a
Guillaume Bouffard <guillaume@getkahoot.com>
2018-03-23 11:46:53 +0000

    Illustrate the use of react-responsive

--- src/responsive-design.js
+++ src/responsive-design.js
@@ -1,10 +1,26 @@
 import React from 'react';
+import MediaQuery from 'react-responsive';
 
 const ResponsiveDesign = () => {
+  const reactResponsiveDiv = (
+    <div
+      className="responsive-design-div">
+      - ideal when a complete different component needs to be rendered<br/>
+      - as opposed to modifying/updating the css.<br/><br/>
+      Example: a basktet ball image will replace that div content at breakpoint 600px.
+    </div>
+  );
+
+  const basketballImage = (
+    <img
+      className="basketball-image"
+      src="./images/basketball.png" />
+  );
+
   return (
     <div>
       <h3>
-        Responsive Design:
+        Responsive Design - Using CSS media queries in CSS:
       </h3>
 
       <img
@@ -17,6 +33,17 @@ const ResponsiveDesign = () => {
       </div>
 
       <h3>
+        Responsive Design - Using React-responsive:
+      </h3>
+
+      <MediaQuery
+        minWidth={600}>
+        {(matches) => {
+          return (matches) ? reactResponsiveDiv : basketballImage;
+        }}
+      </MediaQuery>
+
+      <h3>
         Adaptive Design:
       </h3>
 
@@ -24,6 +51,12 @@ const ResponsiveDesign = () => {
         className="responsive-div">
         Need to to try modernizr browser detection
       </div>
+
+      {/*
+        navigator.userAgent.match(/Firefox/);
+        navigator.userAgent.match(/Chrome/);
+        navigator.userAgent.match(/Safari/);
+      */}
 
       <h3>
         - even if not CSS, html lang="en"
